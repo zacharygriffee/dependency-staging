e13bd7fffcfd79af17e40abb61b51c34916635cb
Zachary Griffee <zacharygriffee@gmail.com>
2024-02-20 18:29:48 -0500

    - Premade dependencies so far, a basic set that I personally use in almost every project - Add browser import map detection in the dependency installation process. - Add dependency.execute which for javascript modules basically just returns the module. The dependency must pass its validation and it must be installed. - fix stage.put not working with an array of dependencies.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import {getStage} from "./lib/stage/getStage.js";
-
+import * as Premade from "./lib/premade-dependencies/index.js";
 const stage = getStage();
+
 const cradle = stage.cradle;
 const {install: stageInstall} = cradle;
-export {cradle as stage, stage as stageContainer, stageInstall as install};
+export {
+    cradle as stage,
+    stage as stageContainer,
+    stageInstall as install,
+    Premade
+};
